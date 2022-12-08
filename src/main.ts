12e18058f0d32087424d8472f8fbc5076287ab85
xtreet <xtreetmall@gmail.com>
2022-12-08 05:36:18 -0800

    second update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet'
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet())
-  app.enableCors()
+  app.use(helmet());
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
